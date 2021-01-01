093109b83c5452f0207b2d8c9733563c92def7e5
blendh <b.hamiti.9@gmail.com>
2021-01-01 12:50:38 +0100

    updated mysql folder

--- mysql/create_statements.sql
+++ mysql/create_statements.sql
@@ -1,10 +1,10 @@
 -- Schema CREATE statement
 
-CREATE DATABASE `workout_planner` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE DATABASE `workout_planner`;
 
 -- Table CREATE statements 
 
-CREATE TABLE `exercises` (
+CREATE TABLE `workout_planner`.`exercises` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(45) NOT NULL,
   `duration` int DEFAULT '0',
@@ -12,20 +12,20 @@ CREATE TABLE `exercises` (
   `sets` int DEFAULT '1',
   PRIMARY KEY (`id`),
   KEY `index_name` (`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+);
 
-CREATE TABLE `routines` (
+CREATE TABLE `workout_planner`.`routines` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(45) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `index_name` (`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+);
 
-CREATE TABLE `routine_exercises` (
+CREATE TABLE `workout_planner`.`routine_exercises` (
   `routine_id` int NOT NULL,
   `exercise_id` int NOT NULL,
   KEY `routine_exercises_fk_exercise_idx` (`exercise_id`),
   KEY `routine_exercises_fk_routine_idx` (`routine_id`),
   CONSTRAINT `routine_exercises_fk_exercise` FOREIGN KEY (`exercise_id`) REFERENCES `exercises` (`id`),
   CONSTRAINT `routine_exercises_fk_routine` FOREIGN KEY (`routine_id`) REFERENCES `routines` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+);
